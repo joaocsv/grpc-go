975083c2404d0893ed05b0fc0cd6cd72d4b92142
João Carlos <joaoc.avils@gmail.com>
2022-08-08 10:52:21 -0300

    Implementando metodo AddUserVerbose

--- app/grpc/services/user_service.go
+++ app/grpc/services/user_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	context "context"
+	"time"
 
 	"github.com/joaocsv/grpc-go/grpc/generators"
 )
@@ -16,4 +17,37 @@ func (*UserService) AddUser(ctx context.Context, request *generators.User) (*gen
 		Name:  request.GetName(),
 		Email: request.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *generators.User, stream generators.UserService_AddUserVerboseServer) error {
+	stream.Send(&generators.UserResultStream{
+		Status: "initial",
+		User:   &generators.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&generators.UserResultStream{
+		Status: "Saving",
+		User: &generators.User{
+			Id:    "0",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&generators.UserResultStream{
+		Status: "Completed",
+		User: &generators.User{
+			Id:    "1",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
